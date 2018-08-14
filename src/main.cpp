255b1b6f81cf9e43eb4f0f94aa8c4039606a9841
milos94 <a.milos94@gmail.com>
2018-08-14 21:47:26 +0200

    Inital commit

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+//Main file
+
+#include <iostream>
+#include "matrix_lib/matrix.h"
+
+int main(int argc, char* argv[])
+{
+    Matrix m;
+    std::cout << "It works\n";
+    return 0;
+}
---
+++ src/matrix_lib/matrix.h
@@ -0,0 +1,16 @@
+//Matrix class for representation of matrix
+
+#ifndef MATRIX_H
+#define MATRIX_H
+
+#include <vector>
+
+using vector = std::vector<int>;
+
+class Matrix
+{
+    vector row;
+
+};
+
+#endif
