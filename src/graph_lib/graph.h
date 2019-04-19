434e0ea451a6e81bfb5ba75e6f29272c04587389
milos94 <a.milos94@gmail.com>
2019-04-19 14:50:54 +0200

    Added type trait for strings

--- src/graph_lib/graph.h
+++ src/graph_lib/graph.h
@@ -27,15 +27,23 @@
 
 #include "graph_lib_base.h"
 #include <vector>
+#include <string>
 #include <algorithm>
+#include <type_traits>
 #include <assert.h>
 
 template <typename V, bool undirected>
 class graph_lib::graph
 {
 private:
-    std::vector<std::pair <V, std::vector<V>>> _adjacency_list;
-    typename std::vector<V>::size_type         _number_of_edges;
+
+    using node_type = std::conditional_t
+                            < std::is_same_v < std::decay<V>, char * >,
+                              std::string,
+                              V >;
+
+    std::vector<std::pair <node_type, std::vector<node_type>>> _adjacency_list;
+    typename std::vector<node_type>::size_type                 _number_of_edges;
 
 public:
     //rule of five
@@ -50,14 +58,14 @@ public:
 
     // returns the number of vertices in G
     [[nodiscard]] constexpr auto num_vertices() const noexcept 
-        -> typename std::vector< std::pair < V, std::vector<V> > >::size_type
+        -> typename std::vector< std::pair < node_type, std::vector<node_type> > >::size_type
     {
         return _adjacency_list.size();
     }
 
     // returns the number of edges in G
     [[nodiscard]] constexpr auto num_edges() const noexcept 
-        -> typename std::vector<V>::size_type
+        -> typename std::vector<node_type>::size_type
     {
         return _number_of_edges;
     }
@@ -66,49 +74,49 @@ public:
     // by default you iterate trough the std::pair<V, std::vector<V>> where v is the vertex and vector contains all of the adjacent vertices to v
     #pragma region iterators
     [[nodiscard]] auto begin() noexcept (noexcept (std::begin(_adjacency_list) ) )  
-        -> typename std::vector< std::pair < V, std::vector<V> > >::iterator
+        -> typename std::vector< std::pair < node_type, std::vector<node_type> > >::iterator
     {
         return std::begin(_adjacency_list);
     }
 
     [[nodiscard]] auto const cbegin() const noexcept (noexcept (std::cbegin(_adjacency_list) ) ) 
-        -> typename std::vector< std::pair < V, std::vector<V> > >::const_iterator
+        -> typename std::vector< std::pair < node_type, std::vector<node_type> > >::const_iterator
     {
         return std::cbegin(_adjacency_list);
     }
 
     [[nodiscard]] auto end() noexcept (noexcept (std::end(_adjacency_list) ) )
-        -> typename std::vector< std::pair <V, std::vector<V> > >::iterator
+        -> typename std::vector< std::pair <node_type, std::vector<node_type> > >::iterator
     {
         return std::end(_adjacency_list);
     }
 
     [[nodiscard]] auto const cend() const noexcept (noexcept (std::cend(_adjacency_list) ) )
-        -> typename std::vector< std::pair < V, std::vector<V> > >::const_iterator
+        -> typename std::vector< std::pair < node_type, std::vector<node_type> > >::const_iterator
     {
         return std::cend(_adjacency_list);
     }
 
     [[nodiscard]] auto rbegin() noexcept (noexcept (std::rbegin(_adjacency_list) ) )
-        -> typename std::vector< std::pair < V, std::vector<V> > >::reverse_iterator
+        -> typename std::vector< std::pair < node_type, std::vector<node_type> > >::reverse_iterator
     {
         return std::rbegin(_adjacency_list);
     }
 
     [[nodiscard]] auto const crbegin() const noexcept (noexcept (std::crbegin(_adjacency_list) ) )
-        -> typename std::vector< std::pair < V, std::vector<V> > >::const_reverse_iterator
+        -> typename std::vector< std::pair < node_type, std::vector<node_type> > >::const_reverse_iterator
     {
         return std::crbegin(_adjacency_list);
     }
 
     [[nodiscard]] auto rend() noexcept (noexcept (std::rend(_adjacency_list) ) )
-        -> typename std::vector< std::pair < V, std::vector<V> > >::reverse_iterator
+        -> typename std::vector< std::pair < node_type, std::vector<node_type> > >::reverse_iterator
     {
         return std::rend(_adjacency_list);
     }
 
     [[nodiscard]] auto const crend() const noexcept (noexcept (std::crend(_adjacency_list) ) ) 
-        -> typename std::vector< std::pair < V, std::vector<V> > >::const_reverse_iterator
+        -> typename std::vector< std::pair < node_type, std::vector<node_type> > >::const_reverse_iterator
     {
         return std::crend(_adjacency_list);
     }
@@ -117,7 +125,7 @@ public:
     // returns the degree of the vertex
     // assets whether the graph contains that vertex
     [[nodiscard]] constexpr auto degree(V vertex) const 
-        -> typename std::vector<V>::size_type
+        -> typename std::vector<node_type>::size_type
     {
         auto const & it = std::find(std::begin(_adjacency_list), std::end(_adjacency_list),
                                                 [&](auto const & i){ return i.first == vertex;});
@@ -129,7 +137,7 @@ public:
     // returns the vector of adjacent verices of the vertex
     // assets whether the graph contains that vertex
     [[nodiscard]] constexpr decltype(auto) adjacent_vertices(V vertex) const
-        -> typename std::vector<V> const &
+        -> typename std::vector<node_type> const &
     {
         auto const & it = std::find(std::begin(_adjacency_list), std::end(_adjacency_list),
                                                 [&](auto const & i){ return i.first == vertex;});
@@ -146,7 +154,7 @@ public:
                                                 [&](auto const & i){ return i.first == vertex;});
         assert(it == std::cend(_adjacency_list));
 
-        _adjacency_list.emplace_back(std::make_pair< V, std::vector<V> >(vertex, std::vector<V>{}) );
+        _adjacency_list.emplace_back(std::make_pair< node_type, std::vector<node_type> >(vertex, std::vector<node_type>{}) );
     }
 
 };
