14effa5a08021485a8db5ef15986d10900d1cdc0
milos94 <a.milos94@gmail.com>
2019-08-18 18:28:06 +0200

    Removed unneeded code from main

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,7 @@
 #include <iostream>
 #include "graph.hpp"
 
-using std::cout;
-
 int main([[maybe_unused]]int argc, [[maybe_unused]] char* argv[])
 {
-    graph_lib::graph<char, true> g{};
-
-    std::cout << "It works \n";
-
     return 0;
 }
